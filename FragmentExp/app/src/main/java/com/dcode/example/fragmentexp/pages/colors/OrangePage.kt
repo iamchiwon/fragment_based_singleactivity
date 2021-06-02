9c3417f05bb97e8ddfff5064cb6a56df1019c1da
iamchiwon <iamchiwon@itsdcode.com>
2021-06-02 11:09:09 +0900

    fail to create generic ViewBindingPage

--- FragmentExp/app/src/main/java/com/dcode/example/fragmentexp/pages/colors/OrangePage.kt
+++ FragmentExp/app/src/main/java/com/dcode/example/fragmentexp/pages/colors/OrangePage.kt
@@ -1,17 +1,25 @@
 package com.dcode.example.fragmentexp.pages.colors
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.dcode.example.fragmentexp.R
 import com.dcode.example.fragmentexp.databinding.PageOrangeBinding
-import com.dcode.example.fragmentexp.system.BaseViewBindinPage
+import com.dcode.example.fragmentexp.system.BasePage
 import com.dcode.example.fragmentexp.system.NavigationPage
 
-class OrangePage : BaseViewBindinPage<PageOrangeBinding>(R.layout.page_orange) {
+class OrangePage : BasePage(R.layout.page_orange) {
+
+    private lateinit var binding: PageOrangeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = PageOrangeBinding.inflate(inflater)
+        return binding.root
+    }
 
     override fun viewDidLoad() {
         super.viewDidLoad()
-
-        binding = PageOrangeBinding.inflate(LayoutInflater.from(context))
 
         binding.internalButton.setOnClickListener {
             navigationPage?.pushPage(PurplePage())
